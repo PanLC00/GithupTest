ecd8f2c39d4dc2c864d5819e05a85510e1580585
PanLC00 <13012637591@163.com>
2023-10-01 14:40:53 +0800

    Initial Commit

---
+++ Memorizes/MemorizesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizesApp.swift
+//  Memorizes
+//
+//  Created by 潘令川 on 2023/10/1.
+//
+
+import SwiftUI
+
+@main
+struct MemorizesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
